e04868931c3f3cd3a4189aea81faa7fc80c085a8
Edyta <edytawesolowska@gmail.com>
2024-10-20 17:24:23 +0200

    zmiany

--- js/app.js
+++ js/app.js
@@ -1,10 +1,19 @@
 const navigation = document.querySelector('.navigation');
 const showNav = document.querySelector(".mobile__nav");
 const closeNav = document.querySelector(".mobile__nav__close");
+const navbar = document.querySelector(".navbar");
 
 const closeMobileMenu = () => navigation.classList.remove('is-active');
 const showMobileMenu = () => navigation.classList.add('is-active');
+const smallMenu = function(){
+    if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80){
+        navbar.classList.add('smallnavbar')
+        
+    } else{
+        navbar.classList.remove('smallnavbar')
 
-
+    }
+}
+document.addEventListener('scroll', smallMenu);
 closeNav.addEventListener('click', closeMobileMenu)
 showNav.addEventListener('click', showMobileMenu)
